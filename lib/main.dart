645f3873d4e9cb2b0e4b042352dd974e98997ef5
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-06-10 11:37:23 +0700

    fix [AR]: main dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               ),
             );
           } else {
-            return const ResponsivePage();
+            return const LoginPage();
           }
         },
       ),
